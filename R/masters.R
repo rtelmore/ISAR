e1bc12da2076f1f835765e3f64c90d11fafc044b
Ryan Elmore <Ryan.Elmore@du.edu>
2020-10-24 21:52:19 -0600

    masters data

---
+++ R/masters.R
@@ -0,0 +1,12 @@
+#' 2019 Masters Tournament Scores
+#'
+#' A dataset containing the scores from the 2019 Masters Golf Tournament.
+#'
+#' @format A data frame with 87 rows and 9 variables:
+#' \describe{
+#'   \item{pos}{finishing position for a player}
+#'   \item{player_name}{name of the golfer}
+#'   ...
+#' }
+#' @source \url{https://www.augusta.com/masters/historic/leaderboards/2019}
+"masters"
