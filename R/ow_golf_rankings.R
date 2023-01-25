3e2bcaa5bd68c9716a8b032d7f9b2cfa2ca1128c
Ryan Elmore <Ryan.Elmore@du.edu>
2023-01-26 10:16:04 +1300

    more golf data sets

---
+++ R/ow_golf_rankings.R
@@ -0,0 +1,11 @@
+#' 2022 Official World Golf Rankings
+#'
+#' A dataset containing the Official World Golf Rankings in June 2022
+#'
+#' @format A data frame with 300 rows and 2 variables:
+#' \describe{
+#'   \item{WGR_June_2022}{official golf world ranking}
+#'   \item{Player_initial_last}{initial of first name and complete last name of player}
+#' }
+#' @source \url{https://www.owgr.com/archive/archive.sps?iType=1747}
+"ow_golf_rankings"
