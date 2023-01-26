50148e6c5a86261fffd9c5fea8a6beeef6851724
Ryan Elmore <Ryan.Elmore@du.edu>
2023-01-26 19:42:38 +1300

    R functions for nhl data

---
+++ R/avs_roster_2021.R
@@ -0,0 +1,17 @@
+#' 2021 NHL Colorado Avalanche Roster (from NHL's API)
+#'
+#' A dataset containing the NHL's Avalanche Roster
+#'
+#' @format A data frame with 28 rows and 8 variables:
+#' \describe{
+#'   \item{jersey_number}{player jersey number}
+#'   \item{person_id}{player ID}
+#'   \item{person_full_name}{player full name}
+#'   \item{person_link}{API link to player data}
+#'   \item{position_code}{position abbreviation}
+#'   \item{position_name}{position name}
+#'   \item{position_type}{position type}
+#'   \item{position_abbreviation}{another position abbreviation}
+#' }
+#' @source \url{https://statsapi.web.nhl.com/api/v1}
+"avs_roster_2021"
